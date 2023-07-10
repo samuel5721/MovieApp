d40c355957b321bf07c0fa390b332fcc59018ff1
Kim Samuel <samuel20070731@gmail.com>
2023-07-10 22:37:59 +0900

    처음 페이지

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
