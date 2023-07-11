686db86b40dcf1cdf59147f06444a0120d7e2410
Kim Samuel <samuel20070731@gmail.com>
2023-07-11 18:37:39 +0900

    Home route로 이관

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Movie({ title, summary, genres, rating, cover_image}) {
---
+++ src/routes/Home.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+import Movie from './components/movie';
+
+function Home() {
+  const [loading, setLoding] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoding(false);
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, []);
+  
+  console.log(movies);
+
+  return (
+    <div className="App">
+      {loading ? <h3>loading...</h3> :
+        movies.map(movie =>
+          <Movie
+            key={movie.id}
+            title={movie.title}
+            summary={movie.summary ?? "none"}
+            genres={movie.genres ?? []}
+            rating={movie.rating ?? "none"}
+            cover_image={movie.medium_cover_image}
+          />
+        )
+      }
+    </div>
+  );
+}
+
+export default Home;
